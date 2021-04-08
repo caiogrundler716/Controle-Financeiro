224a0d1d04c2c0b85ac1ce58ee939dddc7fcc893
Caio Grundler <caiob.grundler2002@gmail.com>
2021-04-08 19:46:43 -0300

    iniciando o script

---
+++ script.js
@@ -0,0 +1,6 @@
+const dummyTransactions = [
+    { id: 1, name: 'Bolo de Chocolate', amount: -20},
+    { id: 2, name: 'Salário', amount: 350},
+    { id: 3, name: 'Torta De Carne', amount: -15},
+    { id: 4, name: 'guitarra', amount: 150}
+]
