14b96b4baca9731e99afeb777c1e06ed91150fdf
Caio Grundler <caiob.grundler2002@gmail.com>
2021-04-09 18:26:07 -0300

    script do codigo

--- script.js
+++ script.js
@@ -2,5 +2,11 @@ const dummyTransactions = [
     { id: 1, name: 'Bolo de Chocolate', amount: -20},
     { id: 2, name: 'Salário', amount: 350},
     { id: 3, name: 'Torta De Carne', amount: -15},
-    { id: 4, name: 'guitarra', amount: 150}
+    { id: 4, name: 'Guitarra', amount: 150}
 ]
+
+const addTransactionIntoDom = transaction => {
+    const operator = transaction.amount < 0 ? '-' : '+'
+
+   
+}
