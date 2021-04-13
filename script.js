7ed218ad91caf06a314c2de84e2c96f1c54fd3f0
Caio Grundler <caiob.grundler2002@gmail.com>
2021-04-13 19:27:07 -0300

    arrumando o script

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 const transactionsUl = document.querySelector('#transactions')
 
 const dummyTransactions = [
-    { id: 1, name: 'Bolo de Chocolate', amount: -20},
+    { id: 1, name: 'Chocolate', amount: -20},
     { id: 2, name: 'Salário', amount: 350},
-    { id: 3, name: 'Torta De Carne', amount: -15},
-    { id: 4, name: 'Guitarra', amount: 150}
+    { id: 3, name: 'Carne', amount: -15},
+    { id: 4, name: 'Piano', amount: 150}
 ]
 
 const addTransactionIntoDom = transaction => {
@@ -18,6 +18,26 @@ const addTransactionIntoDom = transaction => {
     `
     console.log(li)
 
-    transactionsUl.append(li)
+    transactionsUl.prepend(li)
 }
-addTransactionIntoDom(dummyTransactions[0])
+
+const updateBalanceValues = () =>{
+ const transactionAmounts = dummyTransactions
+    .map(transaction => transaction.amount)
+const total = transactionAmounts
+    .reduce((accumalator,transaction) => accumalator + transaction, 0)
+    .toFixed(2)
+const income = transactionAmounts
+    .filter(value => value > 0)
+    .reduce((accumalator, value) => accumalator + value, 0)
+    .toFixed(2)
+    const expense = transactionAmounts.filter(value => value < 0)
+    console.log(expense)
+}
+
+const init = () =>{
+
+    dummyTransactions.forEach(addTransactionIntoDom)
+}
+
+init()
